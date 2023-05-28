6aa568dd37e030ab4c0f9e5d13a963d98ab011db
Nicolas Guerra <52302560+nguerra040@users.noreply.github.com>
2023-05-28 15:38:14 -0500

    Delete ipython_log.py
    File not essential for pipeline

--- ipython_log.py
+++
@@ -1,4 +0,0 @@
-# IPython log file
-
-print('yeet')
-runfile('/home/peizhiliu/Documents/Synced/pyklip_tests/subaru_data/CHARIS_pyKLIP_DRP/pipeline/main.py', wdir='/home/peizhiliu/Documents/Synced/pyklip_tests/subaru_data/CHARIS_pyKLIP_DRP/pipeline')
